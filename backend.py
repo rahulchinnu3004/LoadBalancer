074d9c9e5aff88ecdecd6489127da4fbdfc79309
rahulchinnu3004 <rahulchinnu3004.com>
2026-07-30 22:35:08 +0530

    Added Worker thread and Stats data

--- backend.py
+++ backend.py
@@ -8,21 +8,31 @@ PORT = int(sys.argv[1])
 SERVER_NAME = sys.argv[2]
 
 class MyHandler(BaseHTTPRequestHandler):
+
     def do_GET(self):
 
-        response = f"Hello from {SERVER_NAME} 1"
+        if self.path == "/health":
+            response = "healthy"
+
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain")
+            self.send_header("Content-Length", str(len(response)))
+            self.end_headers()
+            self.wfile.write(response.encode())
+            return
+
+        response = f"Hello from {SERVER_NAME}"
+
         self.send_response(200)
         self.send_header("Content-Type", "text/plain")
         self.send_header("Server", SERVER_NAME)
-        self.send_header("Content-length", str(len(response)))
+        self.send_header("Content-Length", str(len(response)))
         self.end_headers()
         self.wfile.write(response.encode())
 
-
-
-    # Optional: suppress default request logging
     def log_message(self, format, *args):
         print(f"[{SERVER_NAME}] {self.address_string()} - {format % args}")
+
 
 server = HTTPServer(("localhost", PORT), MyHandler)
 
--- server.py
+++ server.py
@@ -1,9 +1,14 @@
 import socket
 import threading
+import time
+import logging
+import json
 from queue import Queue
 
 host = '192.168.1.4'
 port = 8001
+backend_count = -1
+rr_lock = threading.Lock()
 
 request_queue = Queue()
 
@@ -13,24 +18,48 @@ backend_server =[
     ('127.0.0.1',8006)
 ]
 
+backend_stats = {
+    ("127.0.0.1", 8004): {
+        "healthy": True,
+        "requests": 0,
+        "failures": 0,
+        "last_checked": None
+    },
+    ("127.0.0.1", 8005): {
+        "healthy": True,
+        "requests": 0,
+        "failures": 0,
+        "last_checked": None
+    },
+    ("127.0.0.1", 8006): {
+        "healthy": True,
+        "requests": 0,
+        "failures": 0,
+        "last_checked": None
+    }
+}
+
+healthy_servers = [True, True, True]
+
 
 def listen_active_conn():
-    print("listening for active connections on: "+str(host)+":"+str(port))
+    logging.info(f"listening for active connections on: {host}:{port}")
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((host,port))
     server.listen()
     while True:
         client,addr = server.accept()
-        print("received connection from: "+str(addr))
+        logging.info(f"received connection from: {addr}")
         request_queue.put((client,addr))
 
 def resolve_reqeust(conn,req,count):
     backend = socket.socket()
-    print("connecting to backend server: "+str(backend_server[count]))
+    logging.info(f"connecting to backend server: {backend_server[count]}")
     backend.connect(backend_server[count])
-    print("sending request to backend server: "+str(backend_server[count]))
+    logging.info(f"sending request to backend server: {backend_server[count]}")
     backend.sendall(req.encode())
-    print("waiting for response from backend server: "+str(backend_server[count]))
+    logging.info(f"waiting for response from backend server: {backend_server[count]}")
+    backend_stats[backend_server[count]]["requests"] += 1
     header_data = b""
 
     while b"\r\n\r\n" not in header_data:
@@ -51,34 +80,106 @@ def resolve_reqeust(conn,req,count):
     return header + b"\r\n\r\n" + body
 
 def request_handler(conn,count):
-    print("waiting for client request from: "+str(conn.getpeername()))
+    logging.info(f"waiting for client request from: {conn.getpeername()}")
     req = conn.recv(1024).decode()
-    print("received request from client: "+req)
-    print("resolving request from client: "+str(conn.getpeername())+" to backend server: "+str(backend_server[count]))
-    response = resolve_reqeust(conn,req,count)
-    print("sending response to client: "+str(conn.getpeername()))
-    conn.sendall(response)
-    print("closing connection with client: "+str(conn.getpeername()))
-    conn.close()
+    get_request_endpoint = req.split(" ")[1]
+    if get_request_endpoint == "/stats":
+        response = statics_of_servers()
+        logging.info(f"response of stats request from client: {conn.getpeername()} is: {response}")
+        conn.sendall(response)
+        conn.close()
+        return
+    else:
+        logging.info(f"received request from client: {req}")
+        logging.info(f"resolving request from client: {conn.getpeername()} to backend server: {backend_server[count]}")
+        response = resolve_reqeust(conn,req,count)
+        logging.info(f"sending response to client: {conn.getpeername()}")
+        conn.sendall(response)
+        logging.info(f"closing connection with client: {conn.getpeername()}")
+        conn.close()
+
+def get_healthy_server(last_index):
+    n = len(backend_server)
+
+    for _ in range(n):
+        last_index = (last_index + 1) % n
+        if healthy_servers[last_index]:
+            return last_index
+    return None
+
 
 def solve():
-    count = 0
+    global backend_count
     while True:
-        if not request_queue.empty():
-            print("active connections in queue: "+str(request_queue.qsize()))
-            client, addr = request_queue.get()
-            count = count%3
-            request_handler(client,count)
-            count = count+1
-        else:
-            continue
+        logging.info(f"active connections in queue: {request_queue.qsize()}")
+        client, addr = request_queue.get()
+        with rr_lock:
+            backend_count = get_healthy_server(backend_count)
+            selected_backend =backend_count
+        request_handler(client,selected_backend)
+
+def start_worker_threads(num_threads):
+    for thread_id in range(num_threads):
+        thread = threading.Thread(target=solve,name=f"WorkerThread-{thread_id}")
+        thread.start()
 
 
-thread1 = threading.Thread(target=listen_active_conn)
-thread1.start()
+def health_check():
+    while True:
+        for server in backend_server:
+            try:
+                backend = socket.socket()
+                backend.connect(server)
+                backend.sendall(b"GET /health HTTP/1.1\r\nHost: localhost\r\n\r\n")
+                response = backend.recv(1024).decode()
+                if "healthy" in response:
+                    logging.info(f"Backend server {server} is healthy.")
+                    healthy_servers[backend_server.index(server)] = True
+                    backend_stats[server]["healthy"] = True
+                    backend_stats[server]["last_checked"] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+                else:
+                    logging.info(f"Backend server {server} is not healthy.")
+                    healthy_servers[backend_server.index(server)] = False
+                    backend_stats[server]["healthy"] = False
+                    backend_stats[server]["last_checked"] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+                backend.close()
+            except Exception as e:
+                logging.error(f"Error checking health of backend server {server}: {e}")
+                healthy_servers[backend_server.index(server)] = False
+                backend_stats[server]["healthy"] = False
+                backend_stats[server]["last_checked"] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
-thread2 = threading.Thread(target=solve)
-thread2.start()
+        time.sleep(5)
 
 
-# connect()
+def statics_of_servers():
+    logging.info("Fetching statistics of backend servers.")
+    body = {
+        f"{ip}:{port}": data
+        for (ip, port), data in backend_stats.items()
+    }
+    body = json.dumps(body, indent=4).encode()
+    logging.info(f"Statistics of backend servers: {body.decode()}")
+    response = (
+        b"HTTP/1.1 200 OK\r\n"
+        b"Content-Type: application/json\r\n"
+        + f"Content-Length: {len(body)}\r\n".encode()
+        + b"Connection: close\r\n"
+        + b"\r\n"
+        + body
+    )
+    logging.info(f"Response to be sent for /stats request: {response.decode()}")
+    return response
+
+def main():
+
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s',handlers=[logging.FileHandler("load-balancer.log"), logging.StreamHandler()])
+    thread1 = threading.Thread(target=listen_active_conn,name="ListenerThread")
+    thread1.start()
+
+    start_worker_threads(4)
+
+    thread3 = threading.Thread(target=health_check,name="HealthCheckThread")
+    thread3.start()
+
+main()
